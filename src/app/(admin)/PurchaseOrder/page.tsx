edd24fea5e4f780b782e6822cf43aadf885f3a09
vietlucoder <vietleruoi@gmail.com>
2025-11-04 22:12:50 +0700

    u

--- src/app/(admin)/PurchaseOrder/page.tsx
+++ src/app/(admin)/PurchaseOrder/page.tsx
@@ -3,7 +3,7 @@ import PurchaseOrderList from "@/components/purchaseOrders/PurchaseOrderList";
 
 export default function Page() {
    return (
-    <div className="space-y-3">
+    <div className="space-y-3 p-20">
       <PageBreadcrumb pageTitle="Nhập hàng " />
       <PurchaseOrderList />
     </div>
--- src/app/(admin)/suppliers/page.tsx
+++ src/app/(admin)/suppliers/page.tsx
@@ -4,7 +4,7 @@ import SupplierList from "@/components/suppliers/SupplierList";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 export default function Page() {
   return (
-    <div className="p-3">
+    <div className="p-20">
      <PageBreadcrumb pageTitle="Nhà cung cấp" />
       <SupplierList />
     </div>
