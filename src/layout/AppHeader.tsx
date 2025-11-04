c72d7bbfcf7cf0ee5a8c8de0889845b4d87c68b6
Le Nguyen Hoang Phuc <133560614+PhucPhuc1111@users.noreply.github.com>
2025-11-04 22:03:13 +0700

    Update AppHeader.tsx

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -60,7 +60,7 @@ const AppHeader: React.FC = () => {
       title: "Nhập hàng",
       items: [
         { name: "Nhà cung cấp", path: "/suppliers" },
-        { name: "Nhập hàng", path: "/purchaseOrder" },
+        { name: "Nhập hàng", path: "/PurchaseOrder" },
         { name: "Trả hàng nhập", path: "/purchaseReturns" },
       ],
     },
