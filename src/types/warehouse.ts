7cfc5c2b6e19447d4d2ba2bc7b42c5ef84b9816a
vietlucoder <vietleruoi@gmail.com>
2025-11-05 03:17:54 +0700

    clean code fix bug deploy

--- src/types/warehouse.ts
+++ src/types/warehouse.ts
@@ -9,3 +9,19 @@ export interface Warehouse {
   isMainWarehouse: boolean;
   status: string;
 }
+
+export interface WarehouseFormData {
+    id: string;
+  name: string;
+  code: string;
+  address: string;
+  'phone-number': string;
+  'branch-id': string;
+  'is-main-warehouse': boolean;
+  status: 'Hoạt động' | 'Ngưng hoạt động';
+}
+
+export interface Branch {
+  id: string;
+  name: string;
+}
